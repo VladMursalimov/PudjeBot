1e16e523219e7565f32dd84de617fa9a6d5a8651
VladMursalimov <74963780+VladMursalimov@users.noreply.github.com>
2021-04-23 14:52:01 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 settings = {
-    'token': 'ODMwNzYyMjY5NTY2Njk3NDcy.YHLZpw.DuLLo_JYlxZFRSiCKwsIfpE-OUw',
+    'token': 'ODMwNzYyMjY5NTY2Njk3NDcy.YHLZpw.Yp7qpG58O436fjCTjyQfYW1tWXY',
     'bot': 'Pudje',
     'id': 830762269566697472,
     'prefix': 'p '
